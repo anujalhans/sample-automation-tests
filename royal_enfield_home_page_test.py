d12e99c42172220911e80b5bc829bc00a30edbf0
Anuj Alhans <aonu123@gmail.com>
2025-09-29 15:52:36 +0530

    Added Accept Cookies Test Steps

--- royal_enfield_home_page_test.py
+++ royal_enfield_home_page_test.py
@@ -1,6 +1,8 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 @pytest.fixture
 def browser():
@@ -9,16 +11,28 @@ def browser():
     yield driver
     driver.quit()
 
+def accept_cookies_if_present(driver):
+    """Utility to accept cookies if the popup is present"""
+    try:
+        WebDriverWait(driver, 5).until(
+            EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Accept')]"))
+        ).click()
+    except:
+        # No popup found → continue silently
+        pass
+
 
 def test_royal_enfield_title(browser):
     """Check if the page title is correct"""
     browser.get("https://www.royalenfield.com/in/en/home/")
+    accept_cookies_if_present(browser)
     assert "Royal Enfield India | Roadster, Adventure, Cruiser Motorcycles India" in browser.title
 
 
 def test_royal_enfield_logo_visible(browser):
     """Check if the Royal Enfield logo is displayed"""
     browser.get("https://www.royalenfield.com/in/en/home/")
+    accept_cookies_if_present(browser)
     logo = browser.find_element(By.CSS_SELECTOR, "img[alt='Royal Enfield India']")
     assert logo.is_displayed(), "Royal Enfield logo should be visible on the homepage"
 
@@ -26,6 +40,7 @@ def test_royal_enfield_logo_visible(browser):
 def test_motorcycles_navigation(browser):
     """Check if navigation to 'Motorcycles' page works"""
     browser.get("https://www.royalenfield.com/in/en/home/")
+    accept_cookies_if_present(browser)
     motorcycles_link = browser.find_element(By.XPATH, "//a[@title='Motorcycles']//span[@class='desktop-title-text']")
     motorcycles_link.click()
 
